6d6987ce0727397e1b9ca53d15705b05b0393cdf
sagar_pant <sagarpant2k2@gmail.com>
2023-05-02 08:57:39 +0530

    created productSchema

--- models/product.js
+++ models/product.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema ({
-
+    name:{type: String, required: true},
+    description: { type:String, required: true},
+    category : { type:String,required },
+    price : {type:Number,required:true},
+    imageUrl: {type:String,required:true},
+    quantity: {type:Number,default: 0},
 });
 
 module.exports = mongoose.model("Product",productSchema);
