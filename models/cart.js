6ac20b396d1ea5e8cfaeede8eafd8a508b2d3302
sagar_pant <sagarpant2k2@gmail.com>
2023-05-02 08:51:12 +0530

    created cart model

--- models/cart.js
+++ models/cart.js
@@ -1,7 +1,30 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema ({
-    
-})
+    userId: {
+        type:Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [
+        {
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1,
+            },
+            price : {
+                type: Number,
+                required: true
+            }
+        },
+    ],
+},
+{timestamps: true});
 
 module.exports = mongoose.model("Cart",cartSchema);
--- routes/cart.js
+++ routes/cart.js
@@ -3,8 +3,18 @@ const tokenChecker = require('./jwtVerify');
 
 const cart = require('../models/cart');
 
-// Add an item to the cart
+//Get cart items
+router.get('/',tokenChecker,(req,res)=>{
+    const userCart = cart.findOne({
+        userId = req.userId
+    })
+})
 
+// Add an item to the cart
 router.post('/',tokenChecker,async(req,res)={
-    
+    req.userId
 });
+
+
+
+module.exports = router
