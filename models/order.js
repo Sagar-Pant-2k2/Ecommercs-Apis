3ea7bf18b4463dfd4baed06bcdb8db068a0e504d
sagar_pant <sagarpant2k2@gmail.com>
2023-05-02 09:05:44 +0530

    created order model

--- models/order.js
+++ models/order.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema ({
-
-});
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        products : [
+            {
+                product : {
+                    type:Schema.Types.ObjectId,
+                    ref: 'Product',
+                    required: true
+                },
+                quantity: {
+                    type:Number,
+                    required: true
+                }
+            }
+        ],
+        totalAmount : {
+            type: Number,
+            required: true
+        },
+        status: {
+            type:String,
+            enum: ['Pending', 'Delivered', 'Cancelled'],
+            default: 'pending'
+        }
+},
+{timestamps:true});
 
 module.exports = mongoose.model("Order",orderSchema);
