6211f9205ed5dc99e7bc3565ef73dcaa8ebb76bd
sagar_pant <sagarpant2k2@gmail.com>
2023-05-03 10:20:01 +0530

    added functionality to delete particular item from cart

--- routes/cart.js
+++ routes/cart.js
@@ -44,8 +44,25 @@ router.post('/addItems/:productId',tokenChecker,async(req,res)=>{
 //update items in cart
 
 
-// delete items from cart
-
+// delete item from cart
+router.delete('/:id',tokenChecker,async(req,res)=>{
+    try{
+        const userCart = await Cart.findOne({userId :req.userId}).populate('items.product','name quantity price');
+        if(!userCart) { res.status(200).json({message:"Cart is empty already empty"}) }
+        else {
+            const index = userCart.items.findIndex(item=>item.product._id.toString()===req.params.id);
+            if (index !== -1) {
+                userCart.items.splice(index, 1);
+                await userCart.save();
+                res.status(200).json({ message: "Item removed from cart" });
+              } else {
+                res.status(404).json({ message: "Item not found in cart" });
+              }
+        }
+            
+    }
+    catch(err){ res.status(500).json({message:"Can't remove that item"}); }
+});
 
 //emptyCart 
 router.delete('/',tokenChecker,async(req,res)=>{
